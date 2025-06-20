f89005d85976e25f9df7fe18c164d45689d1e691
jjamesliu <liujameszhi@gmail.com>
2025-06-20 16:19:03 -0700

    finished project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,33 +1,30 @@
 import styles from './ProjectCard.module.css'
-
+import reduceCarbonPic from '../assets/reduce_carbon_project.png'
 function ProjectCard() {
  return (
     <>  
-        <section
-        className={styles.container}>
-
+        <section className={styles.container}>
         <h1 className={styles.title}>Projects</h1>
 
         <div className={styles.projectContainer}>
-
-        <div className={styles.projectElement}>
-            <div>
-            <h3>Reduce Carbon</h3>
-            <p className={styles.projectDesc}>
-                A React-based web application that helps users visualize and reduce their carbon emissions based on travel routes. 
-                By integrating the Climatiq API, the app calculates CO₂ impact for different travel options, 
-                allowing users to compare and choose eco-friendlier alternatives. I also implemented geolocation features and interactive 
-                Google Maps to create an engaging and informative user experience.
-            </p>
+            <div className={styles.projectElement}>
+                <div className = {styles.projectImg}>
+                    <img src={reduceCarbonPic}/>
+                </div>
+                <div>
+                <h3 className={styles.projectTitle}>Reduce Carbon</h3>
+                <p className={styles.projectDesc}>
+                    A React-based web application that helps users visualize and reduce their carbon emissions based on travel routes. 
+                    By integrating the Climatiq API, the app calculates CO₂ impact for different travel options, 
+                    allowing users to compare and choose eco-friendlier alternatives. I also implemented geolocation features and interactive 
+                    Google Maps to create an engaging and informative user experience.
+                </p>
+                </div>
             </div>
-        </div>
 
-        <div className={styles.projectElement}>
-            <h3>Currency Converter</h3>
-        </div>
+
 
         </div>
-        
         </section>
     </>
  )
