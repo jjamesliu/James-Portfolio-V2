7eea20cb5bd84bf5d48d325c2f590ccc40e04617
jjamesliu <liujameszhi@gmail.com>
2025-06-27 10:33:20 -0700

    more styling added

--- src/components/Ecommerce.jsx
+++ src/components/Ecommerce.jsx
@@ -3,7 +3,7 @@ import styles from './Ecommerce.module.css'
 function Ecommerce() {
     return (
     <div className={styles.container}> 
-        <a className={styles.link} href="https://jjamesliu.github.io/ecomzhi-portfolio/" target='_blank'>check out my ecommerce portfolio</a>
+        <a className={styles.link} href="https://jjamesliu.github.io/Ecomzhi-Portfolio/" target='_blank'>check out my ecommerce portfolio</a>
     </div>
     )
 }
